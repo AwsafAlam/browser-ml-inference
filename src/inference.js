682f9a865fdbd2cdf133b7fd51d261c1712fa390
AwsafAlam <awsafalam@gmail.com>
2024-09-01 12:01:52 +0600

    Added new classifier model and modifier to accomodate binary classification

--- src/inference.js
+++ src/inference.js
@@ -2,11 +2,12 @@
 /*global BigInt */
 /*global BigInt64Array */
 
-import { loadTokenizer } from './bert_tokenizer.ts';
-import * as wasmFeatureDetect from 'wasm-feature-detect';
+// import { chipClasses } from '@mui/material'
+import { loadTokenizer } from './bert_tokenizer.ts'
+// import * as wasmFeatureDetect from 'wasm-feature-detect'
 
-//Setup onnxruntime 
-const ort = require('onnxruntime-web');
+//Setup onnxruntime
+const ort = require('onnxruntime-web')
 
 //requires Cross-Origin-*-policy headers https://web.dev/coop-coep/
 /**
@@ -23,142 +24,141 @@ const simdResolver = wasmFeatureDetect.simd().then(simdSupported => {
 */
 
 const options = {
-  executionProviders: ['wasm'], 
-  graphOptimizationLevel: 'all'
-};
+	executionProviders: ['wasm'],
+	graphOptimizationLevel: 'all',
+}
 
-var downLoadingModel = true;
-const model = "./xtremedistill-go-emotion-int8.onnx";
+var downLoadingModel = true
+// const model = './xtremedistill-go-emotion-int8.onnx'
+const model = './classifier_int8.onnx'
 
-const session = ort.InferenceSession.create(model, options);
-session.then(t => { 
-  downLoadingModel = false;
-  //warmup the VM
-  for(var i = 0; i < 10; i++) {
-    console.log("Inference warmup " + i);
-    lm_inference("this is a warmup inference");
-  }
-});
+const session = ort.InferenceSession.create(model, options)
+session.then(t => {
+	downLoadingModel = false
+	//warmup the VM
+	for (var i = 0; i < 10; i++) {
+		console.log('Inference warmup ' + i)
+		lm_inference('this is a warmup inference')
+	}
+})
 
 const tokenizer = loadTokenizer()
 
 const EMOJI_DEFAULT_DISPLAY = [
-  ["Emotion", "Score"],
-  ['admiration 👏',0],
-  ['amusement 😂', 0],
-  ['neutral 😐',0],
-  ['approval 👍',0],
-  ['joy 😃',0],
-  ['gratitude 🙏',0],
-];
+	['Emotion', 'Score'],
+	['Non-Toxic 😃', 0],
+	['Toxic 😐', 0],
+]
 
-const EMOJIS = [
-  'admiration 👏',
-  'amusement 😂',
-  'anger 😡',
-  'annoyance 😒',
-  'approval 👍',
-  'caring 🤗',
-  'confusion 😕',
-  'curiosity 🤔',
-  'desire 😍',
-  'disappointment 😞',
-  'disapproval 👎',
-  'disgust 🤮',
-  'embarrassment 😳',
-  'excitement 🤩',
-  'fear 😨',
-  'gratitude 🙏',
-  'grief 😢',
-  'joy 😃',
-  'love ❤️',
-  'nervousness 😬',
-  'optimism 🤞',
-  'pride 😌',
-  'realization 💡',
-  'relief😅',
-  'remorse 😞', 
-  'sadness 😞',
-  'surprise 😲',
-  'neutral 😐'
-];
+const EMOJIS = ['Non-Toxic 😃', 'Toxic 😐']
 
 function isDownloading() {
-  return downLoadingModel;
+	return downLoadingModel
 }
 
 function sortResult(a, b) {
-  if (a[1] === b[1]) {
-      return 0;
-  }
-  else {
-      return (a[1] < b[1]) ? 1 : -1;
-  }
+	if (a[1] === b[1]) {
+		return 0
+	} else {
+		return a[1] < b[1] ? 1 : -1
+	}
 }
 
 function sigmoid(t) {
-  return 1/(1+Math.pow(Math.E, -t));
+	return 1 / (1 + Math.pow(Math.E, -t))
 }
 
+// function argmax(arr) {
+// 	let maxIndex = 0 // Start with the first index
+// 	let maxValue = arr[0] // Start with the first element as the max value
+
+// 	// Iterate over the array starting from the second element
+// 	for (let i = 1; i < arr.length; i++) {
+// 		if (arr[i] > maxValue) {
+// 			// Compare each element with the current max value
+// 			maxValue = arr[i] // Update the max value if a new one is found
+// 			maxIndex = i // Update the index of the max value
+// 		}
+// 	}
+
+// 	return maxIndex // Return the index of the max value
+// }
+
 function create_model_input(encoded) {
-  var input_ids = new Array(encoded.length+2);
-  var attention_mask = new Array(encoded.length+2);
-  var token_type_ids = new Array(encoded.length+2);
-  input_ids[0] = BigInt(101);
-  attention_mask[0] = BigInt(1);
-  token_type_ids[0] = BigInt(0);
-  var i = 0;
-  for(; i < encoded.length; i++) { 
-    input_ids[i+1] = BigInt(encoded[i]);
-    attention_mask[i+1] = BigInt(1);
-    token_type_ids[i+1] = BigInt(0);
-  }
-  input_ids[i+1] = BigInt(102);
-  attention_mask[i+1] = BigInt(1);
-  token_type_ids[i+1] = BigInt(0);
-  const sequence_length = input_ids.length;
-  input_ids = new ort.Tensor('int64', BigInt64Array.from(input_ids), [1,sequence_length]);
-  attention_mask = new ort.Tensor('int64', BigInt64Array.from(attention_mask), [1,sequence_length]);
-  token_type_ids = new ort.Tensor('int64', BigInt64Array.from(token_type_ids), [1,sequence_length]);
-  return {
-    input_ids: input_ids,
-    attention_mask: attention_mask,
-    token_type_ids:token_type_ids
-  }
+	var input_ids = new Array(encoded.length + 2)
+	var attention_mask = new Array(encoded.length + 2)
+	var token_type_ids = new Array(encoded.length + 2)
+	input_ids[0] = BigInt(101)
+	attention_mask[0] = BigInt(1)
+	token_type_ids[0] = BigInt(0)
+	var i = 0
+	for (; i < encoded.length; i++) {
+		input_ids[i + 1] = BigInt(encoded[i])
+		attention_mask[i + 1] = BigInt(1)
+		token_type_ids[i + 1] = BigInt(0)
+	}
+	input_ids[i + 1] = BigInt(102)
+	attention_mask[i + 1] = BigInt(1)
+	token_type_ids[i + 1] = BigInt(0)
+	const sequence_length = input_ids.length
+	input_ids = new ort.Tensor('int64', BigInt64Array.from(input_ids), [
+		1,
+		sequence_length,
+	])
+	attention_mask = new ort.Tensor('int64', BigInt64Array.from(attention_mask), [
+		1,
+		sequence_length,
+	])
+	token_type_ids = new ort.Tensor('int64', BigInt64Array.from(token_type_ids), [
+		1,
+		sequence_length,
+	])
+	return {
+		input_ids: input_ids,
+		attention_mask: attention_mask,
+		token_type_ids: token_type_ids,
+	}
 }
 
 async function lm_inference(text) {
-  try { 
-    const encoded_ids = await tokenizer.then(t => {
-      return t.tokenize(text); 
-    });
-    if(encoded_ids.length === 0) {
-      return [0.0, EMOJI_DEFAULT_DISPLAY];
-    }
-    const start = performance.now();
-    const model_input = create_model_input(encoded_ids);
-    const output =  await session.then(s => { return s.run(model_input,['output_0'])});
-    const duration = (performance.now() - start).toFixed(1);
-    const probs = output['output_0'].data.map(sigmoid).map( t => Math.floor(t*100));
-    
-    const result = [];
-    for(var i = 0; i < EMOJIS.length;i++) {
-      const t = [EMOJIS[i], probs[i]];
-      result[i] = t;
-    }
-    result.sort(sortResult); 
-    
-    const result_list = [];
-    result_list[0] = ["Emotion", "Score"];
-    for(i = 0; i < 6; i++) {
-       result_list[i+1] = result[i];
-    }
-    return [duration,result_list];    
-  } catch (e) {
-    return [0.0,EMOJI_DEFAULT_DISPLAY];
-  }
-}    
+	try {
+		const encoded_ids = await tokenizer.then(t => {
+			return t.tokenize(text)
+		})
+		if (encoded_ids.length === 0) {
+			return [0.0, EMOJI_DEFAULT_DISPLAY]
+		}
+		console.log('Encoded ID:', encoded_ids)
+		const start = performance.now()
+		const model_input = create_model_input(encoded_ids)
+		const output = await session.then(s => {
+			return s.run(model_input, ['output_0'])
+		})
+		const duration = (performance.now() - start).toFixed(1)
+		console.log('output', output)
+		const probs = output['output_0'].data
+			.map(sigmoid)
+			.map(t => Math.floor(t * 100))
+		console.log('probability', probs)
+		const result = []
+		for (var i = 0; i < EMOJIS.length; i++) {
+			const t = [EMOJIS[i], probs[i]]
+			result[i] = t
+		}
+		result.sort(sortResult)
+		console.log(result)
 
-export let inference = lm_inference 
+		const result_list = []
+		result_list.push(['Emotion', 'Score'])
+		for (i = 0; i < result.length; i++) {
+			result_list.push(result[i])
+		}
+		return [duration, result_list]
+	} catch (e) {
+		return [0.0, EMOJI_DEFAULT_DISPLAY]
+	}
+}
+
+export let inference = lm_inference
 export let columnNames = EMOJI_DEFAULT_DISPLAY
 export let modelDownloadInProgress = isDownloading
